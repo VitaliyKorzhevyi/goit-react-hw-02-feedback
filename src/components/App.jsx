b71bbfd10f34b2b66432b70358e3a0e574e0c532
VitaliyKorzhevyi <111433531+VitaliyKorzhevyi@users.noreply.github.com>
2023-03-23 21:02:30 +0200

    HW2React-Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+import { ResetButton } from './RezetBtn/RezetBtn';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = key => {
+    this.setState(prevState => ({ [key]: prevState[key] + 1 }));
+  };
+
+  countTotal = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+  countPotivePercentage = () => {
+    const total = this.countTotal();
+    return Math.round((this.state.good / total) * 100) + '%';
+  };
+
+  handleReset = () => {
+    this.setState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  render() {
+    const { good, bad, neutral } = this.state;
+    const total = this.countTotal();
+    return (
+      <>
+        <Section title={'Please leave a feedback'}>
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleClick}
+          />
+          {total === 0 ? (
+            <Notification/>
+          ) : (
+            <Section title={'Statistics'}>
+              <Statistics
+                good={good}
+                bad={bad}
+                neutral={neutral}
+                total={total}
+                positivePercentage={this.countPotivePercentage()}
+              />
+              <ResetButton onReset={this.handleReset} />
+            </Section>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
